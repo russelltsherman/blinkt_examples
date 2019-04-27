a75436280ef13e8d8cc5f1112b7d2350e1b90e8e
Russell <russell.t.sherman@gmail.com>
2019-04-27 15:35:42 -0500

    colours brightness

--- solid_colours_brightness/app.go
+++ solid_colours_brightness/app.go
@@ -1,36 +1,30 @@
 package main
 
-import . "github.com/russelltsherman/blinkt_go"
+import (
+	"github.com/russelltsherman/blinkt"
+)
 
 func main() {
-
 	brightness := 0.5
-	blinkt := NewBlinkt(brightness)
-
-	blinkt.SetClearOnExit(true)
-
-	blinkt.Setup()
-
-	Delay(100)
-
+	blinky := blinkt.NewBlinkt(brightness)
+	blinky.SetClearOnExit(true)
+	blinky.Setup()
+	blinkt.Delay(100)
 	step := 0
 
 	for {
-
 		step = step % 3
 		switch step {
 		case 0:
-			blinkt.SetAll(128, 0, 0).SetBrightness(0.5)
+			blinky.SetAll(128, 0, 0).SetBrightness(0.5)
 		case 1:
-			blinkt.SetBrightness(0.1).SetAll(0, 128, 0)
+			blinky.SetBrightness(0.1).SetAll(0, 128, 0)
 		case 2:
-			blinkt.SetAll(0, 0, 128).SetBrightness(0.9)
+			blinky.SetAll(0, 0, 128).SetBrightness(0.9)
 		}
 
 		step++
-		blinkt.Show()
-		Delay(500)
-
+		blinky.Show()
+		blinkt.Delay(500)
 	}
-
 }
