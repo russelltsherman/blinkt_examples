b9296e610093ba594c848b40af814dc71c9bcf8b
Russell <russell.t.sherman@gmail.com>
2019-04-27 15:30:14 -0500

    simplify cheerlights

--- cheerlights/app.go
+++ cheerlights/app.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"encoding/hex"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/russelltsherman/blinkt"
 )
@@ -18,6 +25,49 @@ func getEnv(envVar string, assumed int) int {
 
 const defaultRefreshSeconds = 60
 const envRefreshSeconds = "refresh_seconds"
+
+type cheerlight struct {
+	Colour string `json:"field2"`
+}
+
+func getRGBFromColour(hexString string) (int, int, int) {
+	//strip off the hash and decode the remaining hex value
+	hexCol, _ := hex.DecodeString(strings.TrimPrefix(hexString, "#"))
+	//return the ints representing rgb
+	return int(hexCol[0]), int(hexCol[1]), int(hexCol[2])
+}
+
+func getCheerlightColours() (int, int, int) {
+	var netClient = &http.Client{
+		Timeout: time.Second * 3,
+	}
+	resp, getErr := netClient.Get("http://api.thingspeak.com/channels/1417/field/2/last.json")
+
+	if getErr != nil {
+		log.Panic(getErr)
+	}
+
+	if resp.Body != nil {
+		defer resp.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(resp.Body)
+
+	if readErr != nil {
+		log.Panic(getErr)
+	}
+
+	result := cheerlight{}
+
+	parseErr := json.Unmarshal(body, &result)
+	if parseErr != nil {
+		log.Panic("Can't parse response")
+		return 0, 0, 0
+	}
+
+	return getRGBFromColour(result.Colour)
+
+}
 
 func main() {
 	brightness := 0.5
